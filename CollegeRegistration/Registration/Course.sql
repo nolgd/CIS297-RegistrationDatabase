f31d1dc89aac1332ede74ac4bfa7456f51d73d77
nolgd <39465135+nolgd@users.noreply.github.com>
2019-04-25 21:30:53 -0400

    Revert "anal"
    This reverts commit d5006c3dd27eded0358ed311f8326fb0420974f8.

---
+++ CollegeRegistration/Registration/Course.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Course]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Number] NVARCHAR(10) NOT NULL, 
+    [Credits] INT NOT NULL, 
+    [Department] NCHAR(10) NOT NULL
+)
